9b55a35d990b1fab83830f820bd6164318dcd5cb
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-03-19 20:39:09 +0100

    Update Project

--- VM/Gestion_Pharma/Produits/urls.py
+++ VM/Gestion_Pharma/Produits/urls.py
@@ -3,4 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('produit/', Affichage.as_view(), name='home'),
+     path('produit/modifier/<int:pk>/', views.modifier_produit, name='modifier'),
+    
+    # URL pour supprimer un produit
+    path('produit/supprimer/<int:pk>/', views.supprimer_produit, name='delete'),
+    
+    # URL pour voir les détails d'un produit
+    path('produit/details/<int:pk>/', views.details_produit, name='details'),
 ]
