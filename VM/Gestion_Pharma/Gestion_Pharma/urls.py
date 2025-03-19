8a3d61c9d2be98f33a6773d3577566ce44246195
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-03-19 02:51:47 +0100

    Update Project

--- VM/Gestion_Pharma/Gestion_Pharma/urls.py
+++ VM/Gestion_Pharma/Gestion_Pharma/urls.py
@@ -1,26 +1,10 @@
-"""
-URL configuration for Gestion_Pharma project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Gestion_Pharma/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from Produits import views 
+from Produits import views  # Importer les vues de l'application Produits
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Produits/', include('Produits.urls')),
-    path('', views.home, name='home'),  
+    path('Produits/', include('Produits.urls')),  # Inclure les URLs de l'application Produits
+    path('', views.Affichage.as_view(), name='home'),  # Utilisation de la classe Affichage pour l'URL ''
 ]
-
